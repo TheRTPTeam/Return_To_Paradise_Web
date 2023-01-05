3bc20e3997df1764287e30ad1ecfe844eb195bf9
Sabine Lim <thesabinelim@gmail.com>
2023-01-05 23:03:07 +1100

    Set up module path for components/

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,20 +1,25 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
   webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.s[ac]ss$/i,
-      use: [
-        // Creates `style` nodes from JS strings
-        "style-loader",
-        // Translates CSS into CommonJS
-        "css-loader",
-        // Compiles Sass to CSS
-        "sass-loader",
-      ]
-    });
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ];
     return config;
   },
 };
