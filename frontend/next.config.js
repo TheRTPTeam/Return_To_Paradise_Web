1bf5d0cca921d054454348bcba6f906bb337b40d
Sabine Lim <thesabinelim@gmail.com>
2023-01-06 00:03:30 +1100

    Uninstall sass-loader as Next supports Sass

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,20 +6,6 @@ const nextConfig = {
     appDir: true,
   },
   webpack: (config, options) => {
-    config.module.rules = [
-      ...config.module.rules,
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-    ];
     return config;
   },
 };
