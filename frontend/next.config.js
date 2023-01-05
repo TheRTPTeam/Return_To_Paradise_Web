9df565434a16793a63c99c1e390ec9066605e34a
Sabine Lim <thesabinelim@gmail.com>
2023-01-05 21:53:32 +1100

    Install SASS

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.s[ac]ss$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        "style-loader",
+        // Translates CSS into CommonJS
+        "css-loader",
+        // Compiles Sass to CSS
+        "sass-loader",
+      ]
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
